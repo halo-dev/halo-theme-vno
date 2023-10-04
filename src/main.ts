aaf377ec8a5c6035475b585111127c0ab950cc56
Hilary Liu <2788370451@qq.com>
2023-10-04 20:54:24 +0800

    refactor: adapt to Halo 2.0 (#9)

---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import "./_sass/vno.scss";
+import "animate.css/source/_vars.css";
+import "animate.css/source/_base.css";
+/* Bouncing entrances  */
+import "animate.css/source/bouncing_entrances/bounceIn.css";
+import "animate.css/source/bouncing_entrances/bounceInDown.css";
+import "animate.css/source/bouncing_entrances/bounceInLeft.css";
+import "animate.css/source/bouncing_entrances/bounceInRight.css";
+import "animate.css/source/bouncing_entrances/bounceInUp.css";
+/* Bouncing exits  */
+import "animate.css/source/bouncing_exits/bounceOut.css";
+import "animate.css/source/bouncing_exits/bounceOutDown.css";
+import "animate.css/source/bouncing_exits/bounceOutLeft.css";
+import "animate.css/source/bouncing_exits/bounceOutRight.css";
+import "animate.css/source/bouncing_exits/bounceOutUp.css";
+import "font-awesome/css/font-awesome.css";
