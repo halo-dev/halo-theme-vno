049b4938f19c24be8c4d034e8395178ad15c8b00
Ryan Wang <i@ryanc.cc>
2023-10-04 20:32:38 +0800

    Refactor social media
    Signed-off-by: Ryan Wang <i@ryanc.cc>

--- src/main.ts
+++ src/main.ts
@@ -13,4 +13,4 @@ import "animate.css/source/bouncing_exits/bounceOutDown.css";
 import "animate.css/source/bouncing_exits/bounceOutLeft.css";
 import "animate.css/source/bouncing_exits/bounceOutRight.css";
 import "animate.css/source/bouncing_exits/bounceOutUp.css";
-import "font-awesome/scss/font-awesome.scss";
+import "font-awesome/css/font-awesome.css";
